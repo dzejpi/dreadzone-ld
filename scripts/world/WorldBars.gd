d683bcd0f0576b1eb2c06a6d7eb3e9846359546e
DzejPi <dzejjpii@gmail.com>
2024-10-05 23:12:57 +0200

    Bars can be toggled on and off now.

---
+++ scripts/world/WorldBars.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+
+@export var are_bars_open = false
+
+var closed_position_y = 0.0
+var open_position_y = -4.0
+var slide_time = 1.0
+
+
+func toggle_door():
+	are_bars_open = not are_bars_open
+
+
+func _process(delta: float) -> void:
+	var target_y = open_position_y if are_bars_open else closed_position_y
+	var current_y = transform.origin.y
+	
+	var new_y = lerp(current_y, target_y, delta * slide_time)
+	var new_position = transform.origin
+	new_position.y = new_y
+	transform.origin = new_position
