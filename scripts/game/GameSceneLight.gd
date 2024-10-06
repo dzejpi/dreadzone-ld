8003ca398d0fa2481831e9f714d5755f5aa26787
DzejPi <dzejjpii@gmail.com>
2024-10-06 17:56:05 +0200

    Emergency change - normal level has way too many objects to be able to rendered in HTML5. One arena with enless play will be had.

---
+++ scripts/game/GameSceneLight.gd
@@ -0,0 +1,118 @@
+extends Node3D
+
+
+@onready var a_bars_1: Node3D = $GameWorld/ArenaA/Doors/WorldBars1
+@onready var a_bars_2: Node3D = $GameWorld/ArenaA/Doors/WorldBars2
+
+@onready var a_spawners: Node3D = $GameWorld/ArenaA/Spawners
+
+@onready var beginning_arena_a: Area3D = $BeginningArenaA
+
+@onready var player_scene: CharacterBody3D = $CharacterBodies/PlayerScene
+
+var is_arena_a_clear = false
+
+var is_player_in_arena_a = false
+
+var elapsed_time_a = 0.0
+var trigger_time_a = 0
+
+var elapsed_time_b = 0.0
+var trigger_time_b = 0
+
+var elapsed_time_c = 0.0
+var trigger_time_c = 0
+
+var elapsed_time_d = 0.0
+var trigger_time_d = 0
+
+
+func _ready():
+	# Enter all entrance doors
+	a_bars_1.toggle_door()
+
+
+func _process(delta: float) -> void:
+	if is_player_in_arena_a and !is_arena_a_clear:
+		manage_arena_a_events(delta)
+
+
+func _on_beginning_arena_a_body_entered(body: Node3D) -> void:
+	if body.name == "PlayerScene":
+		if !is_player_in_arena_a:
+			is_player_in_arena_a = true
+			a_bars_1.toggle_door()
+
+
+func spawn_everywhere(which_spawner, creature_number):
+	match(which_spawner):
+		"a":
+			var spawners = a_spawners.get_children()
+			for child in spawners:
+				if child.has_method("spawn_creature"):
+					child.spawn_creature(creature_number)
+
+
+# Function to spawn creature on every Nth spawner
+func spawn_on_nth(which_spawner, nth, creature_number):
+	match(which_spawner):
+		"a":
+			var spawners = a_spawners.get_children()
+			for i in range(spawners.size()):
+				if i % nth == 0:
+					var child = spawners[i]
+					if child.has_method("spawn_creature"):
+						child.spawn_creature(creature_number)
+
+
+func turn_all_spawners_on(which_spawner, creature_number):
+	match(which_spawner):
+		"a":
+			var spawners = a_spawners.get_children()
+			for child in spawners:
+				if child.has_method("spawn_creature"):
+					child.creature_to_spawn = creature_number
+					child.continuous_spawn = true
+
+
+func turn_all_spawners_off(which_spawner):
+	match(which_spawner):
+		"a":
+			var spawners = a_spawners.get_children()
+			for child in spawners:
+				if child.has_method("spawn_creature"):
+					child.continuous_spawn = false
+
+
+func manage_arena_a_events(delta):
+	elapsed_time_a += delta
+	
+	if trigger_time_a != int(elapsed_time_a):
+		trigger_time_a = int(elapsed_time_a)
+		trigger_arena_a_event(trigger_time_a)
+		print("A trigger time: " + str(trigger_time_a))
+
+
+func trigger_arena_a_event(event_triggered):
+	match(event_triggered):
+		5:
+			spawn_on_nth("a", 8, 2)
+		10:
+			spawn_on_nth("a", 4, 2)
+		20:
+			spawn_on_nth("a", 4, 6)
+		30:
+			spawn_on_nth("a", 4, 6)
+		40:
+			spawn_on_nth("a", 2, 6)
+		60:
+			is_arena_a_clear = true
+			a_bars_2.toggle_door()
+
+
+func heal_player(amount):
+	player_scene.receive_health(amount)
+
+
+func give_player_gun(gun_number):
+	player_scene.gain_gun(gun_number)
